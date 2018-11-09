baacd6a99e191cab046fd91e5283c37838f030d6
MariuszAnuszkiewicz <mariusx14@wp.pl>
2018-11-09 21:09:13 +0100

    update project

--- classes/DB.php
+++ classes/DB.php
@@ -5,21 +5,21 @@ use MariuszAnuszkiewicz\Config\Config;
 class DB
 {
     const CONNECT_FAILED = "Failed to connect to Database";
-    private static $_instance;
-    private $_pdo,
-            $_query,
-            $_error = false,
-            $_results,
-            $_count = 0;
+    private static $instance;
+    private $pdo,
+            $query,
+            $error = false,
+            $results,
+            $count = 0;
 
     public function __construct() {
 
         $username = Config::get('db.user');
         $password = Config::get('db.password');
-        $dsn='mysql:dbname='. Config::get('db.db_name') . ';host=' . Config::get('db.host') .'';
+        $dsn = 'mysql:dbname='. Config::get('db.db_name') . ';host=' . Config::get('db.host') .'';
 
         try{
-            $this->_pdo = new \PDO($dsn, $username, $password);
+            $this->pdo = new \PDO($dsn, $username, $password);
         }catch(\PDOException $e) {
             echo self::CONNECT_FAILED;
             die($e->getMessage());
@@ -27,44 +27,44 @@ class DB
     }
 
     public static function getInstance() {
-        if (!isset(self::$_instance)) {
-            self::$_instance = new DB();
+        if (!isset(self::$instance)) {
+            self::$instance = new DB();
         }
-        return self::$_instance;
+        return self::$instance;
     }
 
     public function query($sql, $params = array()) {
 
         if (!empty($params)) {
-            $this->_error = false;
-            if ($this->_query = $this->_pdo->prepare($sql)) {
+            $this->error = false;
+            if ($this->query = $this->pdo->prepare($sql)) {
                 $x = 1;
                 if (count($params)) {
                     if (is_array($params) || is_object($params) || is_string($params) || is_numeric($params)) {
                         foreach ($params as $param) {
-                            $this->_query->bindValue($x, $param);
+                            $this->query->bindValue($x, $param);
                             $x++;
                         }
                     }
                 }
 
-                if ($this->_query->execute()) {
-                    $this->_results = $this->_query->fetchAll(\PDO::FETCH_ASSOC);
-                    $this->_count = $this->_query->rowCount();
+                if ($this->query->execute()) {
+                    $this->results = $this->query->fetchAll(\PDO::FETCH_ASSOC);
+                    $this->count = $this->query->rowCount();
                 } else {
-                    $this->_error = true;
+                    $this->error = true;
                 }
             }
             return $this;
 
-        }  else {
-               $this->_error = false;
-            if ($this->_query = $this->_pdo->prepare($sql)) {
-                if ($this->_query->execute()) {
-                   $this->_results = $this->_query->fetchAll(\PDO::FETCH_ASSOC);
-                   $this->_count = $this->_query->rowCount();
+        } else {
+               $this->error = false;
+            if ($this->query = $this->pdo->prepare($sql)) {
+                if ($this->query->execute()) {
+                   $this->results = $this->query->fetchAll(\PDO::FETCH_ASSOC);
+                   $this->count = $this->query->rowCount();
                 } else {
-                   $this->_error = true;
+                   $this->error = true;
                 }
             }
             return $this;
@@ -72,18 +72,18 @@ class DB
     }
 
     public function getExecute() {
-        return $this->_query->execute();
+        return $this->query->execute();
     }
 
     public function countRow() {
-        return $this->_count;
+        return $this->count;
     }
 
     public function results() {
-        return $this->_results;
+        return $this->results;
     }
 
     public function error() {
-        return $this->_error;
+        return $this->error;
     }
 }
