b5673f18aed76ccfc6d1a8ecfabd6efe1673c11a
MariuszAnuszkiewicz <mariusx14@wp.pl>
2018-11-10 13:15:50 +0100

    update project

---
+++ config/Files.php
@@ -0,0 +1,20 @@
+<?php namespace MariuszAnuszkiewicz\Config;
+
+class Files extends Setting
+{
+    public static function get($key)
+    {
+        return isset(self::$settings[$key]) ? self::$settings[$key] : null;
+    }
+
+    public static function set($key, $value)
+    {
+        self::$settings[$key] = $value;
+    }
+}
+
+// save file
+
+Files::set('link_save_file', $_SERVER['REQUEST_URI'] . "/../../web/uploads/data.json");
+Files::set('save_file',  __DIR__ . "/../web/uploads/data.json");
+
